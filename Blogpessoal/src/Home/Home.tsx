706f13044fb7087891352dbd2b37da861b2416a8
Édipo Reis <ediporeiss@gmail.com>
2024-08-02 15:09:29 -0300

    update

--- Blogpessoal/src/Home/Home.tsx
+++ Blogpessoal/src/Home/Home.tsx
@@ -1,11 +1,10 @@
-import React from "react";
 import './Home.css';
 
 function Home(){
     return(
         <>
-            <h1 className="titulo">Home</h1>
-           <p className="img">BEM VINDO</p>
+            <h1 className="titulo">Inicio</h1>
+          <img src="https://media.licdn.com/dms/image/D4D03AQEsaiL-C-xFTA/profile-displayphoto-shrink_800_800/0/1720186430687?e=1727913600&v=beta&t=gkELRh1i7lel5cdegWFo3kBVatFuQv0G_4YqY4z_VDA" alt="Foto"  className='img'/>
             <body>
                 
             </body>
