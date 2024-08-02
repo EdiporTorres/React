e2771af3ea59a79c26775bde92d338d080a5f344
Édipo Reis <ediporeiss@gmail.com>
2024-08-02 13:35:30 -0300

    Home

---
+++ Blogpessoal/src/Home/Home.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './Home.css';
+
+function Home(){
+    return(
+        <>
+            <h1 className="titulo">Home</h1>
+           <p className="img">BEM VINDO</p>
+            <body>
+                
+            </body>
+        </>
+    );
+}
+
+
+export default Home;
