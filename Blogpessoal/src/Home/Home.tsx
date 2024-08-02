e09c8aef00c8a6fe4633f2dc5640f8384e302a6d
Édipo Reis <ediporeiss@gmail.com>
2024-08-02 15:44:34 -0300

    Branch para testes do Édipo

--- Blogpessoal/src/Home/Home.tsx
+++ Blogpessoal/src/Home/Home.tsx
@@ -3,7 +3,7 @@ import './Home.css';
 function Home(){
     return(
         <>
-            <h1 className="titulo">Inicio</h1>
+            <h1 className="titulo"> <a href="/Blogpessoal/public/Index.html">Inicio</a></h1>
           <img src="https://media.licdn.com/dms/image/D4D03AQEsaiL-C-xFTA/profile-displayphoto-shrink_800_800/0/1720186430687?e=1727913600&v=beta&t=gkELRh1i7lel5cdegWFo3kBVatFuQv0G_4YqY4z_VDA" alt="Foto"  className='img'/>
             <body>
                 
