f18b5d33b39eb5be546ffaba792e7433b997af60
Édipo Reis <ediporeiss@gmail.com>
2024-08-06 20:19:18 -0300

    teste

--- Blogpessoal/src/App.tsx
+++ Blogpessoal/src/App.tsx
@@ -1,10 +1,13 @@
-import Home from "./Home/Home";
+import React from 'react';
 import './App.css';
 
-function App(){
-  return(
-    <Home/>
-  );
+function App() {
+  return (
+    <>
+    <h1 className="text-3xl font-bold text-red-500 underline text-center">
+      Hello world!
+    </h1>
+    </>
+);
 }
-
 export default App;
--- Blogpessoal/src/Home/Home.tsx
+++ Blogpessoal/src/Home/Home.tsx
@@ -1,16 +1,27 @@
-import './Home.css';
-
-function Home(){
-    return(
+function Home() {
+    return (
         <>
-            <h1 className="titulo">Inicio</h1>
-          <img src="https://media.licdn.com/dms/image/D4D03AQEsaiL-C-xFTA/profile-displayphoto-shrink_800_800/0/1720186430687?e=1727913600&v=beta&t=gkELRh1i7lel5cdegWFo3kBVatFuQv0G_4YqY4z_VDA" alt="Foto"  className='img'/>
-            <body>
-                
-            </body>
-        </>
+        <div className="bg-indigo-900 flex justify-center">
+          <div className='container grid grid-cols-2 text-white'>
+            <div className="flex flex-col gap-4 items-center justify-center py-4">
+              <h2 className='text-5xl font-bold'>Seja bem vindo!</h2>
+              <p className='text-xl'>Expresse aqui seus pensamentos e opniões</p>
+  
+              <div className="flex justify-around gap-4">
+              
+                <button className='rounded bg-white text-blue-800 py-2 px-4'>Ver postagens</button>
+              </div>
+            </div>
+  
+            <div className="flex justify-center ">
+              <img src={""} alt="" className='w-2/3' />
+      
+            </div>
+          </div>
+        </div>
+      
+      </>
     );
 }
-
 
 export default Home;
